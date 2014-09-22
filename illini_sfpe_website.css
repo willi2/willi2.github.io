d370c37f45926df7c2b5b208ae6ac34385c179dc
willi2 <willijm92@gmail.com>
2014-09-21 20:57:03 -0500

    website

---
+++ illini_sfpe_website.css
@@ -0,0 +1,205 @@
+html, body {
+	background-image: url("green-fibers.png");
+	margin: 0;
+	padding: 0;
+}
+
+.sticky {
+    margin: 0;
+    padding: 0;
+	position: fixed;
+	width: 100%;
+	top: 0;
+}
+
+.navbar {
+	background-color:rgba(68, 68, 68, 0.7);
+	height: 60px;
+}
+
+nav {
+    margin: 30px 10px 0 0;
+    position: absolute;
+    right: 170px;
+    font-size: 20px;
+    padding: 0;
+}
+
+a:link {
+    font-family: 'Oxygen', sans-serif;
+    color: rgb(231, 100, 32); 
+    text-decoration: none;
+    padding-left: 10px;
+}
+
+a:visited {
+    color: rgb(231, 100, 32);
+    text-decoration: none;
+}
+
+a:hover {
+    color: white;
+}
+
+a:active {
+    color: silver;
+}
+
+.logo {
+    border-top: solid 2px rgb(58, 64, 64);
+    border-bottom: solid 2px rgb(58, 64, 64);
+    height: 82px;
+    padding: 5px 0 0 0;
+    background-image: url('fireFinal01.png');
+}
+
+.logo h1 {
+    padding-left: 180px;
+    font-weight: 300;
+}
+
+.logo h4 {
+    size: 30px;
+    padding: 2px 0 0 348px;
+    color: rgb(58, 64, 64);
+    font-family: 'Comfortaa', sans-serif;
+}
+.logo a {
+    display: block;
+    padding: 0;
+}
+.box {
+   background-color: rgba(162, 162, 162, 0.3);
+   padding-bottom: 1px;
+   margin: 0 150px;
+}
+
+.top img {
+    padding: 100px 0 165px 0;
+    width: 100%;
+}
+
+.main {
+    margin-left: 150px;
+    margin-right: 150px;
+}
+
+img {
+    width: 100%;
+}
+.section {
+}
+.section-wrapper {
+    box-sizing: border-box;
+    height: 900px;
+    padding-bottom: 100px;
+    padding-top: 100px;
+}
+
+.section-wrapper.contact {
+    height: 275px;
+    padding-bottom: 0;
+}
+.title {
+    color: rgb(231, 100, 32);
+    font-family: 'Oxygen', sans-serif;
+    font-weight: 100;
+    font-size: 30px;
+    padding-bottom: 3px;
+    margin: 5px;
+}
+
+p, h1, h4 {
+    margin: 5px;
+    color: white;
+    font-family: 'Oxygen', sans-serif;
+}
+
+p {
+    font-size: 16px;
+}
+.eoh {
+    width: 50%;
+}
+/*
+.left-div {
+    display: inline-block;
+    width: 50%;
+    margin: 0;
+    padding: 0;
+}
+
+.space {
+    display: inline-block;
+    width: 3%;
+}
+
+.right-div {
+    display: inline-block;
+    width: 44%;
+    padding: 44%;
+    padding: 0;
+    margin: 0;
+    vertical-align: top;
+}
+*/
+
+
+
+iframe {
+    border: solid 10px rgb(58, 64, 64);
+    font-family: 'Comfortaa', sans-serif;
+    width:75%; 
+    height:250px;
+}
+
+.officer {
+    display: inline-block;
+    margin-right: 10px;
+    width: 20%;
+}
+.officer img {
+    width: 100%;
+}
+
+.officer h4, .officer p {
+    text-align: center;
+    margin: 0;
+}
+
+.moreinfo a:link {
+ color: white; 
+}
+
+.moreinfo a:visited {
+    color: white;
+    text-decoration: none;
+}
+
+.moreinfo a:hover {
+    color: rgb(231, 100, 32);
+}
+
+.moreinfo a:active {
+    color: silver;
+}
+
+.bottom-image {
+    margin:0;
+    padding:0;
+    max-width: 100%; 
+}
+
+.foot {
+    border-top: solid 3px;
+    border-top-color:rgba(68, 68, 68, 0.8);
+    background-image: url("fireFooter.png");
+    height: 15px;
+    border-bottom: 0px;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    width: 100%; 
+    left: 0;
+    bottom: 0;
+}
