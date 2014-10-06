0ddcafbf54a97058f67ce3361b1cf0488cdf4eae
sghelm2 <sghelm2@illinois.edu>
2014-10-05 21:23:05 -0500

    Update illini_sfpe_website.js

--- illini_sfpe_website.js
+++ illini_sfpe_website.js
@@ -1,205 +1,195 @@
-html, body {
-	background-image: url("green-fibers.png");
-	margin: 0;
-	padding: 0;
-}
 
-.sticky {
-    margin: 0;
-    padding: 0;
-	position: fixed;
-	width: 100%;
-	top: 0;
-}
-
-.navbar {
-	background-color:rgba(68, 68, 68, 0.7);
-	height: 60px;
-}
-
-nav {
-    margin: 30px 10px 0 0;
-    position: absolute;
-    right: 170px;
-    font-size: 20px;
-    padding: 0;
-}
-
-a:link {
-    font-family: 'Oxygen', sans-serif;
-    color: rgb(231, 100, 32); 
-    text-decoration: none;
-    padding-left: 10px;
-}
-
-a:visited {
-    color: rgb(231, 100, 32);
-    text-decoration: none;
-}
-
-a:hover {
-    color: white;
-}
-
-a:active {
-    color: silver;
-}
-
-.logo {
-    border-top: solid 2px rgb(58, 64, 64);
-    border-bottom: solid 2px rgb(58, 64, 64);
-    height: 82px;
-    padding: 5px 0 0 0;
-    background-image: url('fireFinal01.png');
-}
-
-.logo h1 {
-    padding-left: 180px;
-    font-weight: 300;
-}
-
-.logo h4 {
-    size: 30px;
-    padding: 2px 0 0 348px;
-    color: rgb(58, 64, 64);
-    font-family: 'Comfortaa', sans-serif;
-}
-.logo a {
-    display: block;
-    padding: 0;
-}
-.box {
-   background-color: rgba(162, 162, 162, 0.3);
-   padding-bottom: 1px;
-   margin: 0 150px;
-}
-
-.top img {
-    padding: 100px 0 165px 0;
-    width: 100%;
-}
-
-.main {
-    margin-left: 150px;
-    margin-right: 150px;
-}
-
-img {
-    width: 100%;
-}
-.section {
-}
-.section-wrapper {
-    box-sizing: border-box;
-    height: 900px;
-    padding-bottom: 100px;
-    padding-top: 100px;
-}
-
-.section-wrapper.contact {
-    height: 275px;
-    padding-bottom: 0;
-}
-.title {
-    color: rgb(231, 100, 32);
-    font-family: 'Oxygen', sans-serif;
-    font-weight: 100;
-    font-size: 30px;
-    padding-bottom: 3px;
-    margin: 5px;
-}
-
-p, h1, h4 {
-    margin: 5px;
-    color: white;
-    font-family: 'Oxygen', sans-serif;
-}
-
-p {
-    font-size: 16px;
-}
-.eoh {
-    width: 50%;
-}
 /*
-.left-div {
-    display: inline-block;
-    width: 50%;
-    margin: 0;
-    padding: 0;
-}
 
-.space {
-    display: inline-block;
-    width: 3%;
-}
+SMINT V1.0 by Robert McCracken
+SMINT V2.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
+SMINT V3.0 by robert McCracken with some awesome help from Ryan Clarke (@clarkieryan) and mcpacosy ‏(@mcpacosy)
 
-.right-div {
-    display: inline-block;
-    width: 44%;
-    padding: 44%;
-    padding: 0;
-    margin: 0;
-    vertical-align: top;
-}
+SMINT is my first dabble into jQuery plugins!
+
+http://www.outyear.co.uk/smint/
+
+If you like Smint, or have suggestions on how it could be improved, send me a tweet @rabmyself
+
 */
 
 
+(function () {
 
-iframe {
-    border: solid 10px rgb(58, 64, 64);
-    font-family: 'Comfortaa', sans-serif;
-    width:75%; 
-    height:250px;
-}
+	$.fn.smint = function( options ) {
 
-.officer {
-    display: inline-block;
-    margin-right: 10px;
-    width: 20%;
-}
-.officer img {
-    width: 100%;
-}
+		var settings = $.extend({
+			'scrollSpeed'  : 500,
+			'mySelector'     : 'div'
+		}, options);
 
-.officer h4, .officer p {
-    text-align: center;
-    margin: 0;
-}
+		// adding a class to users div
+		$(this).addClass('smint');				
+		
+		//Set the variables needed
+		var optionLocs = new Array(),
+			lastScrollTop = 0,
+			menuHeight = $(".smint").height(),
+			smint = $('.smint'),
+        	smintA = $('.smint a'),
+        	myOffset = smint.height();
 
-.moreinfo a:link {
- color: white; 
-}
+      
 
-.moreinfo a:visited {
-    color: white;
-    text-decoration: none;
-}
 
-.moreinfo a:hover {
-    color: rgb(231, 100, 32);
-}
 
-.moreinfo a:active {
-    color: silver;
-}
+		if ( settings.scrollSpeed ) {
+				var scrollSpeed = settings.scrollSpeed
+			}
 
-.bottom-image {
-    margin:0;
-    padding:0;
-    max-width: 100%; 
-}
+		if ( settings.mySelector ) {
+				var mySelector = settings.mySelector
+		};
 
-.foot {
-    border-top: solid 3px;
-    border-top-color:rgba(68, 68, 68, 0.8);
-    background-image: url("fireFooter.png");
-    height: 15px;
-    border-bottom: 0px;
-    margin: 0;
-    padding: 0;
-    position: fixed;
-    width: 100%; 
-    left: 0;
-    bottom: 0;
-}
+
+
+		return smintA.each( function(index) {
+            
+			var id = $(this).attr('href').split('#')[1];
+
+			if (!$(this).hasClass("extLink")) {
+				$(this).attr('id', id);
+			}
+
+			
+			//Fill the menu
+			optionLocs.push(Array(
+				$(mySelector+"."+id).position().top-menuHeight, 
+				$(mySelector+"."+id).height()+$(mySelector+"."+id).position().top, id)
+			);
+
+			///////////////////////////////////
+
+			// get initial top offset for the menu 
+			var stickyTop = smint.offset().top;	
+
+			// check position and make sticky if needed
+			var stickyMenu = function(direction){
+
+				// current distance top
+				var scrollTop = $(window).scrollTop()+myOffset; 
+
+				// if we scroll more than the navigation, change its position to fixed and add class 'fxd', otherwise change it back to absolute and remove the class
+				if (scrollTop > stickyTop+myOffset) { 
+					smint.css({ 'position': 'fixed', 'top':0,'left':0 }).addClass('fxd');
+
+					// add padding to the body to make up for the loss in heigt when the menu goes to a fixed position.
+					// When an item is fixed, its removed from the flow so its height doesnt impact the other items on the page
+					$('body').css('padding-top', menuHeight );	
+				} else {
+					smint.css( 'position', 'relative').removeClass('fxd'); 
+					//remove the padding we added.
+					$('body').css('padding-top', '0' );	
+				}   
+
+				// Check if the position is inside then change the menu
+				// Courtesy of Ryan Clarke (@clarkieryan)
+				if(optionLocs[index][0] <= scrollTop && scrollTop <= optionLocs[index][1]){	
+					if(direction == "up"){
+						$("#"+id).addClass("active");
+						$("#"+optionLocs[index+1][2]).removeClass("active");
+					} else if(index > 0) {
+						$("#"+id).addClass("active");
+						$("#"+optionLocs[index-1][2]).removeClass("active");
+					} else if(direction == undefined){
+						$("#"+id).addClass("active");
+					}
+					$.each(optionLocs, function(i){
+						if(id != optionLocs[i][2]){
+							
+							$("#"+optionLocs[i][2]).removeClass("active");
+						}
+					});
+				}
+			};
+
+			// run functions
+			stickyMenu();
+
+			// run function every time you scroll
+			$(window).scroll(function() {
+				//Get the direction of scroll
+				var st = $(this).scrollTop()+myOffset;
+				if (st > lastScrollTop) {
+				    direction = "down";
+				} else if (st < lastScrollTop ){
+				    direction = "up";
+				}
+				lastScrollTop = st;
+				stickyMenu(direction);
+
+				// Check if at bottom of page, if so, add class to last <a> as sometimes the last div
+				// isnt long enough to scroll to the top of the page and trigger the active state.
+
+				if($(window).scrollTop() + $(window).height() == $(document).height()) {
+	       			smintA.removeClass('active')
+	       			$(".smint a:not('.extLink'):last").addClass('active')
+	       			
+   				} else {
+   					smintA.last().removeClass('active')
+   				}
+			});
+
+			///////////////////////////////////////
+        
+        	$(this).on('click', function(e){
+				// gets the height of the users div. This is used for off-setting the scroll so the menu doesnt overlap any content in the div they jst scrolled to
+				var myOffset = smint.height();   
+
+        		// stops hrefs making the page jump when clicked
+				e.preventDefault();
+				
+				// get the hash of the button you just clicked
+				var hash = $(this).attr('href').split('#')[1];
+
+				
+
+				var goTo =  $(mySelector+'.'+ hash).offset().top-myOffset;
+				
+				// Scroll the page to the desired position!
+				$("html, body").stop().animate({ scrollTop: goTo }, scrollSpeed);
+				
+				// if the link has the '.extLink' class it will be ignored 
+		 		// Courtesy of mcpacosy ‏(@mcpacosy)
+				if ($(this).hasClass("extLink"))
+                {
+                    return false;
+                }
+
+			});	
+
+
+			//This lets yo use links in body text to scroll. Just add the class 'intLink' to your button and it will scroll
+
+			$('.intLink').on('click', function(e){
+				var myOffset = smint.height();   
+
+				e.preventDefault();
+				
+				var hash = $(this).attr('href').split('#')[1];
+
+				if (smint.hasClass('fxd')) {
+					var goTo =  $(mySelector+'.'+ hash).position().top-myOffset;
+				} else {
+					var goTo =  $(mySelector+'.'+ hash).position().top-myOffset*2;
+				}
+				
+				$("html, body").stop().animate({ scrollTop: goTo }, scrollSpeed);
+
+				if ($(this).hasClass("extLink"))
+                {
+                    return false;
+                }
+
+			});	
+		});
+
+	};
+
+	$.fn.smint.defaults = { 'scrollSpeed': 500, 'mySelector': 'div'};
+})(jQuery);
